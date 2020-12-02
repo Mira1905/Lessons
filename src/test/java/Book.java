24f5748a13c5cee1b6b1abd357ba0b5302a0690c
qa.mira <qa.mira@alldigitalads.com>
2020-12-03 00:14:43 +0200

    Домашнее задание.

---
+++ src/test/java/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+
+     String nameOfTheBook1 = "StudyJava";
+     String nameOfTheBook2 = "ExploringTheWorld";
+     byte pageJava = 100;
+     int page1World = 32000;
+
+}
---
+++ src/test/java/StudyJava.java
@@ -0,0 +1,9 @@
+public class StudyJava {
+
+public static void main(String[]args){
+    Book book = new Book();
+    System.out.println("Я читаю книгу "+book.nameOfTheBook1 + " на "+ book.pageJava + " странице");
+    System.out.println("На досуге дочитываю книгу "+book.nameOfTheBook2 + " на "+ book.page1World + " странице");
+
+}
+}
