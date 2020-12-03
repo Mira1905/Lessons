eb6457c12f35572c83e29cbd43124594d1d2eef9
qa.mira <qa.mira@alldigitalads.com>
2020-12-03 10:10:13 +0200

    Homework. Changes have been made 2.

--- src/test/java/Book.java
+++ src/test/java/Book.java
@@ -1,6 +1,6 @@
 public class Book {
 
-     String nameOfTheBook1 = "Reading"; //переменная с типом string - сылочная
+     String nameOfTheBook1 = "StudyJava"; //переменная с типом string - сылочная
      String nameOfTheBook2 = "ExploringTheWorld"; //переменная с типом string - сылочная
      byte pageJava = 100; //переменная с типом byte - примитивные
      int page1World = 32000; //переменная с типом int - примитивные
